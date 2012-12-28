293063cb68d90c9b0214ec174699413ddaeec34f
Christian Weiske <cweiske@cweiske.de>
2012-12-29 00:26:38 +0100

    allTests.sh to run all tests at once

---
+++ tests/allTests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+dir="`dirname "$0"`"
+cd "`readlink -f "$dir"`"
+
+HAS_ERROR=0
+for i in */*Test.sh; do
+    "./$i"
+    if [ $? -ne 0 ]; then
+        HAS_ERROR=1
+    fi
+done
+
+if [ $HAS_ERROR -eq 0 ]; then
+    echo "all OK"
+else
+    echo "some ERROR"
+fi
+
+exit $HAS_ERROR
